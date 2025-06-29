2e55cfa20a5e32d52fa073de7900be010595428e
TheCoder30ec4 <varun30ec4@gmail.com>
2025-06-29 15:02:03 +0530

    commit

--- output_video_20250625_224656/scenes/step_1_scene.py
+++
@@ -1,9 +0,0 @@
-
-from manim import *
-
-class ErrorScene(Scene):
-    def construct(self):
-        text = Text("Error generating animation")
-        self.play(Write(text))
-        self.wait(2)
-        
